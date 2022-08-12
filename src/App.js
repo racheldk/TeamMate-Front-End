3c7d4233bada961a9b9ce7ada15409d31b99dc9f
Rachel Kelly <racheldk126@gmail.com>
2022-08-12 16:05:18 -0700

    basic routes defined, no components yet

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
-import './App.css';
+import "./App.css";
+import { BrowserRouter, Route } from "react-router-dom";
 
 function App() {
-  return (
-    <div>
-      <h1>TeamMate!</h1>
-    </div>
-  );
+    return (
+        <BrowserRouter>
+            <div>
+                <h1>TeamMate!</h1>
+                <Route path="/" />
+                {/* All Open Games (Game List component? separate component?) */}
+                <Route path="/new" />
+                {/* make a new open game post  */}
+                <Route path="register" />
+                {/* register new user */}
+                <Route path="login" />
+                {/* login */}
+                <Route path= "/mygames" />
+                {/* my games - confirmed, pending requests as guest, pending requests as host, open */}
+                <Route/>
+                {/* This will be for the user profile, but I need to look at how that works again  */}
+
+            </div>
+        </BrowserRouter>
+    );
 }
 
 export default App;
