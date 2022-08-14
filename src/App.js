acda1952f8517da4881a427deefe646d36e06f1a
Rachel Kelly <racheldk126@gmail.com>
2022-08-14 06:50:07 -0700

    removed unused variables so netflify deploy will work

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import "./App.css";
-import { BrowserRouter, Route, Routes, Link } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import OpenGamesList from "./pages/openGamesPage";
 import { useEffect, useState } from "react";
 
