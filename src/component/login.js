01328a1fce037e307dc6a325f0b1553195a459f3
Rachel Kelly <racheldk126@gmail.com>
2022-08-14 06:43:36 -0700

    formatting with prettier

--- src/component/login.js
+++ src/component/login.js
@@ -1,24 +1,38 @@
 function Login() {
-    return(
-        <div className='Login' style={{ textAlign: "center"}}>
-        <div className="form-login">
-        
-        <h1>Please Login</h1>
-        <label className="sr-only">User name</label>
-        <input id="inputUsername" class="form-control" placeholder="User name" required autofocus/>
-        <br/> 
-        <label for="inputPassword" class="sr-only">Password</label>
-        <input type="password" id="inputPassword" class="form-control" placeholder="Password" required/>
-        {/* <div className="checkbox mb-3">
+    return (
+        <div className="Login" style={{ textAlign: "center" }}>
+            <div className="form-login">
+                <h1>Please Login</h1>
+                <label className="sr-only">User name</label>
+                <input
+                    id="inputUsername"
+                    class="form-control"
+                    placeholder="User name"
+                    required
+                    autofocus
+                />
+                <br />
+                <label for="inputPassword" class="sr-only">
+                    Password
+                </label>
+                <input
+                    type="password"
+                    id="inputPassword"
+                    class="form-control"
+                    placeholder="Password"
+                    required
+                />
+                {/* <div className="checkbox mb-3">
             <label>
             <input type="checkbox" value="remember-me" />Remember me
             </label>
         </div> */}
-        <br/>
-        <button class="btn btn-lg btn-primary btn-block" type="submit">Sign in</button>
-    
-    </div>
-    </div>
-    )
-    }
-    export default Login;
+                <br />
+                <button class="btn btn-lg btn-primary btn-block" type="submit">
+                    Sign in
+                </button>
+            </div>
+        </div>
+    );
+}
+export default Login;
