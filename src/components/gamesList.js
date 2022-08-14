44a8e21b4067ff80d17fdb9230e3c22572053e06
Rachel Kelly <racheldk126@gmail.com>
2022-08-13 18:12:27 -0700

    GamesList modal to show game details using Modali - not working correctly with the map, always shows the modal for the last thing mapped

--- src/components/gamesList.js
+++ src/components/gamesList.js
@@ -1,10 +1,12 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
-import { DateTime } from "luxon";
+import Modali, { useModali } from "modali";
 
 export default function GamesList({ token }) {
     const [allGamesList, setAllGamesList] = useState([]);
-    const [date, setDate] = useState()
+    const [openGameModal, toggleOpenGameModal] = useModali();
+    const [modalData, setModalData] = useState(null)
+
 
     useEffect(() => {
         axios
@@ -19,22 +21,37 @@ export default function GamesList({ token }) {
             });
     }, [token, setAllGamesList]);
 
-    useEffect(() => {
-        setDate('01/02/2023')
-    }, [])
-
-
     return (
         <div>
             <h1>GamesList</h1>
             {allGamesList.map((game) => (
-                <div className='game'>
-                    <button>+</button>
+                <div className="game">
+                    <button onClick={() => {
+                        toggleOpenGameModal()
+                        setModalData(game)
+                    }}>+</button>
                     <div>Location: {game.location_info.park_name}</div>
-                    <div>{date.toLocaleString} at {game.time}</div>
+                    <div>
+                        {game.date} at {game.time}
+                    </div>
                     <button>Join</button>
                 </div>
             ))}
+                    <Modali.Modal game={modalData}
+                    {...openGameModal}>
+                        <div>
+                            <div>{modalData.location_info.park_name}</div>
+                            <div>{modalData.location_info.court_surface}</div>
+                            <div>park address will go here too</div>
+                            <div>
+                                {modalData.time} {modalData.date}
+                            </div>
+                            <div>{modalData.host_info.first_name}</div>
+                            <div>{modalData.host_info.last_name}</div>
+                            <div>{modalData.host_info.username}</div>
+                            <button>Join</button>
+                        </div>
+                    </Modali.Modal>
         </div>
     );
 }
