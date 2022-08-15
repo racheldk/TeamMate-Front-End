7feaa4da090ea2fa877af797aab25090c43d5489
Rachel Kelly <racheldk126@gmail.com>
2022-08-15 13:51:49 -0700

    merge conflicts

--- src/App.js
+++ src/App.js
@@ -1,15 +1,7 @@
-<<<<<<< HEAD
-import "./App.css";
-import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
-import NewOpenGame from "./pages/newOpenGame";
-import { useEffect } from "react";
-import useLocalStorageState from "use-local-storage-state";
-=======
 import './App.css';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import NewOpenGame from './pages/newOpenGame';
 import { useEffect, useState } from 'react'
->>>>>>> main
 import OpenGamesList from "./pages/openGamesPage";
 import { ChakraProvider } from "@chakra-ui/react";
 import Login from "./pages/login.js";
@@ -18,48 +10,6 @@ import Theme from "./components/theme";
 import { Text } from "@chakra-ui/react";
 
 function App() {
-<<<<<<< HEAD
-    const [token, setToken] = useLocalStorageState("teammateToken", null);
-    // const [token, setToken] = useState()
-
-//     useEffect(() => {
-//         setToken("ea54a8e01ca1479e2ba6b5aa99ea04dc434b5298");
-//     }, []);
-
-    return (
-        <ChakraProvider Theme={Theme} Text={Text}>
-            <BrowserRouter>
-                {/* <header className="">
-                        <div className="container">
-                        <div className="buttons-header">
-                        <Link to="/login"  className="btn">Login</Link>
-                        <Link to="/register" className="btn">Register</Link>
-                        </div>
-                        </div>
-                </header> */}
-
-                <Routes>
-                    <Route path="/" element={<Login token={token} />} />
-                    {/* All Open Games (Game List component? separate component?) */}
-                    <Route
-                        path="/new"
-                        element={<NewOpenGame token={token} />}
-                    />
-                    {/* make a new open game post  */}
-                    <Route path="register" element={<Register />} />
-                    {/* register new user */}
-                    <Route
-                        path="open-games"
-                        element={<OpenGamesList token={token} />}
-                    />
-                    {/* login */}
-                    <Route path="my-games" />
-                    {/* my games - confirmed, pending requests as guest, pending requests as host, open */}
-                    <Route path=":username" />
-                    {/* This will be a user profile (Team Quokka did something like this with the users/:id route)  */}
-                </Routes>
-            </BrowserRouter>
-=======
   const [token, setToken] = useState()
 
 useEffect(()=>{
@@ -84,7 +34,6 @@ useEffect(()=>{
                 {/* This will be a user profile (Team Quokka did something like this with the users/:id route)  */}
             </Routes>
         </BrowserRouter>
->>>>>>> main
         </ChakraProvider>
     );
 }
