7944dfe1f131839835eef07616da97b5d6519a7d
Rachel Kelly <racheldk126@gmail.com>
2022-08-17 14:49:27 -0700

    filter by location working (endpoint currently down so got an empty array as response)

--- src/pages/openGamesPage.js
+++ src/pages/openGamesPage.js
@@ -4,15 +4,65 @@ import Footer from "../components/FooterMenu";
 import axios from "axios";
 import { useEffect, useState } from "react";
 
-export default function OpenGamesList({ token, listType, setListType, allGamesList }) {
-setListType("allOpen")
+export default function OpenGamesList({
+    token,
+    listType,
+    setListType,
+    allGamesList,
+}) {
+    const [filterLoc, setFilterLoc] = useState(null);
+    const [filteredGames, setFilteredGames] = useState([]);
+
+    setListType("allOpen");
+
+    const handleFilterGameLoc = (event) => {
+        console.log(event.target.value);
+        setFilterLoc(`park-name=${event.target.value}`);
+        console.log(filterLoc);
+    };
+
+    useEffect(() => {
+        console.log(filterLoc);
+        axios
+        .get(`https://teammate-app.herokuapp.com/session?${filterLoc}`, {
+            headers: {
+                Authorization: `Token ${token}`,
+            },
+        })
+        .then((res) => {
+            console.log(res.data)
+            setFilteredGames(res.data)
+        })
+        .catch((error) => {
+            console.log(error)
+            alert(error.response.data.detail)
+        })
+    }, [token, filterLoc]);
 
     return (
         <>
             <Header />
             <div className="app-body">
                 <h1>Open Games List</h1>
-                <GamesList token={token} games={allGamesList} listType={listType} />
+
+                <div>
+                    <div>Filter Games</div>
+                    <select
+                        onChange={handleFilterGameLoc}
+                        value={filterLoc}
+                        id="filter-location"
+                        name="filter-location"
+                    >
+                        <option value="Pullen Park">Pullen Park</option>
+                        <option value="Sanderford Park">Sanderford Park</option>
+                    </select>
+                </div>
+
+                <GamesList
+                    token={token}
+                    games={allGamesList}
+                    listType={listType}
+                />
             </div>
             <Footer />
         </>
