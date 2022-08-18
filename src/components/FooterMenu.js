3405645b9c99518dc940b73c99136821a698100d
samrespass <samrespass@gmail.com>
2022-08-18 10:01:29 -0400

    footer finalized

--- src/components/FooterMenu.js
+++ src/components/FooterMenu.js
@@ -1,16 +1,19 @@
 import { IconButton} from '@chakra-ui/react'
 import { Link } from "react-router-dom";
-import { BsFillPersonFill } from "react-icons/bs"
+import { BsFillPersonFill, BsFillHouseFill } from "react-icons/bs"
 import {SearchIcon, PlusSquareIcon, Icon} from '@chakra-ui/icons'
+import useLocalStorageState from "use-local-storage-state";
 
 
 
 function Footer () {
+    const [username, setUsername] = useLocalStorageState("teammateUsername", null)
+
     return (
             <div className="footer">
-               <Link to='/'><IconButton aria-label='Search Item' fontSize='1.9em' colorScheme='teal' border= 'none' variant='outline' className='footer-button' icon={<SearchIcon />} /></Link>
+               <Link to='/open-games'><IconButton aria-label='Search Item' fontSize='1.9em' colorScheme='teal' border= 'none' variant='outline' className='footer-button' icon={<BsFillHouseFill />} /></Link>
                <Link to='/new'><IconButton aria-label='ProfileItem' fontSize='1.9em' colorScheme='teal' border= 'none' variant='outline' className='footer-button' icon={<PlusSquareIcon/>} /></Link>
-               <Link to='/'><IconButton aria-label='ProfileItem' fontSize='1.9em' colorScheme='teal' border= 'none' variant='outline' className='footer-button' icon={<Icon as={BsFillPersonFill} />} /></Link>
+               <Link to={`/${username}`}><IconButton aria-label='ProfileItem' fontSize='1.9em' colorScheme='teal' border= 'none' variant='outline' className='footer-button' icon={<Icon as={BsFillPersonFill} />} /></Link>
             </div>
        
     )
