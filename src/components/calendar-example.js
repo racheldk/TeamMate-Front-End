20a542cf64d650253b1822d211f4430c9a2e9a0a
Rachel Kelly <racheldk126@gmail.com>
2022-08-20 13:43:04 -0700

    added sample data, event not showing up on calendar

--- src/components/calendar-example.js
+++ src/components/calendar-example.js
@@ -1,24 +1,36 @@
-import { Calendar, momentLocalizer } from 'react-big-calendar'
-import { luxonLocalizer } from 'react-big-calendar'
-import { DateTime, Settings } from 'luxon'
-import 'react-big-calendar/lib/css/react-big-calendar.css'
-import { chakra, Box } from '@chakra-ui/react'
+import { Calendar } from "react-big-calendar";
+import { luxonLocalizer } from "react-big-calendar";
+import { DateTime } from "luxon";
+import "react-big-calendar/lib/css/react-big-calendar.css";
+import { Box } from "@chakra-ui/react";
+import { useState } from "react";
 
+const localizer = luxonLocalizer(DateTime, { firstDayOfWeek: 7 });
 
-const localizer = luxonLocalizer(DateTime, { firstDayOfWeek: 7 })
+const events = [
+    {
+        title: "Event 1",
+        allDay: true,
+        start: new Date(2022, 8, 26),
+        end: new Date(2022, 8, 26),
+        resource: null,
+    },
+];
 
-export default function CalendarExample (props) {
-    
+export default function CalendarExample(props) {
+    // const [eventList, setEventList] = useState([])
+    // console.log(events);
+    // setEventList(events)
+
     return (
-
-  <Box height={400} backgroundColor='white'>
-    {/* Container element around Calender needs to have height specified for it to show up on the page */}
-
-    <Calendar
-      localizer={localizer}
-    //   events={myEventsList}
-      startAccessor="start"
-      endAccessor="end"
-    />
-  </Box>
-)}
+        <Box height={400} backgroundColor="white">
+            {/* Container element around Calender needs to have height specified for it to show up on the page */}
+            <Calendar
+                localizer={localizer}
+                events={events}
+                startAccessor="start"
+                endAccessor="end"
+            />
+        </Box>
+    );
+}
