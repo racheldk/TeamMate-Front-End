1f07867806f48f8390bf7659a2c03056082b446d
Carlos <carlosaltirado@gmail.com>
2022-08-22 19:18:42 -0500

    survey skeleton

--- src/pages/survey.js
+++ src/pages/survey.js
@@ -13,32 +13,39 @@ return (
         <h1>Survey</h1>
 
 
-        <div className="survey-text" >
+        <div className="survey-text">
             <p>Did "user" come to the game?:</p>
+            <Button variant="ghost"colorScheme="teal">YES</Button>
+            <Button variant="ghost"colorScheme="teal">NO</Button>
             <br/>
             <p>Who won?:</p>
+            <Button variant="ghost"colorScheme="teal">Me</Button>
+            <Button variant="ghost"colorScheme="teal">Username</Button>
             <br/>
-            <Button type="button" class="btn btn-primary">Next</Button>
+            <Button marginLeft="100px" width="100px" height="30px" variant="ghost"
+            colorScheme="teal" backgroundColor="teal" color="white">Next</Button>
             <br/>
             <br/>
             <p>Would you play again with "user" again?:</p>
+            <Button variant="ghost"colorScheme="teal">YES</Button>
+            <Button variant="ghost"colorScheme="teal">NO</Button>
             <br/>
             <p>How do you rate the Court Quality?:</p>
-            <div className="footer">
+                <div className="icon-survey">
                 <IconButton aria-label='ProfileItem' fontSize='2.5em' colorScheme='green' border= 'none' 
                 variant='outline' className='footer-button' icon={<BsFillEmojiSmileFill/>} />
                 <IconButton aria-label='ProfileItem' fontSize='2.5em' colorScheme='yellow' border= 'none' 
                 variant='outline' className='footer-button' icon={<BsFillEmojiNeutralFill/>} />
                 <IconButton aria-label='Search Item' fontSize='2.5em' colorScheme='red' border= 'none' 
                 variant='outline' className='footer-button' icon={<BsEmojiFrownFill />} />
-            </div>
+                </div>
             
+            <Button marginLeft="100px" width="100px" height="30px" variant="ghost"
+            colorScheme="teal" backgroundColor="teal" color="white">Submit</Button>
         </div>
         </div>
-        <div class="card-footer text-end">
-        <button type="button" class="btn btn-primary">Submit</button>
         
-    </div>
+
 </div>
 )}
 
