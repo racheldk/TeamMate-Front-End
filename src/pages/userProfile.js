f377cc36fa9ca1e320c267474557ea226066c09c
samrespass <samrespass@gmail.com>
2022-08-29 11:42:20 -0400

    users first and last name show, can edit

--- src/pages/userProfile.js
+++ src/pages/userProfile.js
@@ -117,17 +117,19 @@ function UserProfile({ token, setToken }) {
                     <>
                         <Box className="profile-body" marginTop={4}>
                             <Box className="user-name">
-                                <Heading size="2xl" color="white">
-                                    {user.username}
+                            <Heading size='2xl' color="white">{user.first_name} {user.last_name}</Heading>
+                                <Heading size="lg" color="white">
+                                    @{user.username}&nbsp;
                                     <IconButton
                                         aria-label="ProfileEdit"
                                         o
                                         onClick={() => {
                                             handleOpenEditModal();
                                         }}
-                                        fontSize=".5em"
+                                        fontSize=".7em"
                                         colorScheme="teal"
                                         border="none"
+                                        size='sm'
                                         variant="solid"
                                         icon={
                                             <Icon as={BsPencil} color="white" />
