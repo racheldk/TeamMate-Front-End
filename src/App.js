25493e377a0c38907fd0228ec6521e0cb295187e
Rachel Kelly <racheldk126@gmail.com>
2022-08-30 13:21:54 -0700

    filter bug fixed

--- src/App.js
+++ src/App.js
@@ -112,6 +112,7 @@ function App() {
                             <OpenGamesPage
                                 token={token}
                                 allGamesList={allGamesList}
+                                setAllGamesList
                                 username={username}
                                 setGame={setGame}
                                 game={game}
